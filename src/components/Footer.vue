9a4f3852ff345443a5ee2f348d559c9956bd010b
Joker <14821269+zhufacai@users.noreply.github.com>
2022-09-11 15:49:58 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,6 @@
     <a-back-top />
     <span>Copyright &copy; 2022 <a target="_blank" href="https://github.com/ikxin/kms-tools">KMS Tools</a> | </span>
     <span>Code with by <a target="_blank" href="https://www.ikxin.com">一纸忘忧</a></span>
-    <span v-if="beian">
-      | <a target="_blank" href="http://beian.miit.gov.cn/">{{ beian }}</a></span
-    >
   </a-layout-footer>
 </template>
 
